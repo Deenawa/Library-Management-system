cd6c72590315ff34c6a2ca132c616967b3fc5a54
Muhammad Abdullah Shakoor <116428468+m-abdullah-shakoor@users.noreply.github.com>
2023-05-18 20:03:09 +0500

    Update SCDA3.java

--- src/scd/a3/SCDA3.java
+++ src/scd/a3/SCDA3.java
@@ -15,7 +15,7 @@ public class SCDA3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.Println("Initialization....")
+        System.out.Println("Initialization....");
     }
     
 }
